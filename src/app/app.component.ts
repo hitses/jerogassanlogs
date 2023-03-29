288dd680881b1788ffd24eb0cdb80091474dd268
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-03-29 10:05:07 +0200

    A - Entrada de la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { LogoComponent } from './shared/logo/logo.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet, LogoComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'jerogassan-logs';
